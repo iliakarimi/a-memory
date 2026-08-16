60adb547dc9769a0cb7641b5f3209d6e7edbc7ae
iliakarimi <theiliakarimi@gmail.com>
2026-08-16 19:03:22 +0330

    Nothing for now

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-import json
-import sqlite3
-
 
 
 class Memory():
-    def __init__(self, text="", image=None):
-        self.text = text
-        self.image = image
+    def __init__(self):
+        pass
