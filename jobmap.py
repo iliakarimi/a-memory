8e2e2313ec2248730dee83648bd5bcdc844ac39f
iliakarimi <theiliakarimi@gmail.com>
2026-09-01 18:18:16 +0330

    Add comments and Docstring

--- jobmap.py
+++ jobmap.py
@@ -1,4 +1,19 @@
 class JobMap():
+    '''
+    ## Job Map
+    This class can add A map to agent when it want to do A job.
+    
+    ### methods:
+    * addjob:
+        Agent should sort a map before do A job like:\n
+            a>b>c>... 
+    
+    * usejob:
+        It's A simple method that return one job after calling it and then remove used jobs.
+
+    for more information checkout **A-memory** docs. 
+    '''
+
     def __init__(self, data=None):
         self.job = []
 
@@ -18,7 +33,6 @@ class JobMap():
                 self.job.append(words)
                 num+=1
                 words=''
-        # return self.job
 
     def usejob(self):
         try:
@@ -32,6 +46,6 @@ class JobMap():
         except Exception as e:
             return f"An Unexpected error: {e}"
 
-    def checkjob(self): # Temporary Method
+    # def checkjob(self): # Temporary Method
 
-        return self.job
+    #     return self.job
--- shortmemory.py
+++ shortmemory.py
@@ -1,4 +1,18 @@
 class GoalsMem():
+    '''
+    ## Goals Memory
+    This Class store Chat goals and and facts
+    #### Methods:
+    * set_goals:\n
+        This method can just Store goals and facts in chat
+    
+    * resault:\n
+        This methed can return Stored data
+    
+        
+    for more information checkout **A-memory** docs.
+    '''
+
     def __init__(self):
         self.context_goals = []
         self.recent_goal = None
@@ -7,10 +21,9 @@ class GoalsMem():
         self.cn = 0
         self.fn = 0
 
-    def set_goals(
-            self, contextgoals=None, recentgoal=None, 
-            activegoal=None, chatfacts=None
-            ) -> str:
+    # This method set and store goals in list 
+    def set_goals(self, contextgoals:str | None, recentgoal:str | None, 
+            activegoal:str | None, chatfacts:str | None) -> str:
 
         if activegoal:
             self.active_goal = activegoal
@@ -26,10 +39,9 @@ class GoalsMem():
             self.chat_facts.append(f"number{self.fn}: "+chatfacts)
             self.fn=+1
 
-    def resault(
-            self, cg=False, ag=False, 
-            rg=False, cf=False, al=False
-            ):
+    # The resault methed return data stored in lists
+    def resault(self, cg:bool | False, ag:bool | False, 
+            rg:bool | False, cf:bool | False, al:bool | False):
         resu = []
 
         if cg == True:
@@ -51,19 +63,36 @@ class GoalsMem():
 
 
 class ShortMem():
+    '''
+    ## Short Memory
+    This class is A **Short-term-Memory** for agents Store **Agent** and **user** chat.
+
+    ### methods:
+    * store_messages:\n
+        this method store agent and user messages and remove 
+        first old messages after 8 message betwinn user and agent.
+    
+    * remind_messages:\n
+        This method just return stored messages.
+    
+
+    for more information checkout **A-memory** docs.
+    '''
+
     def __init__(self):
         self.messages = []
         self.message_number = 0
-
+    # Storing messages with 8 limit and forget first and old message after add new message
     def store_messages(self, role:str | None, message:str | None) -> str:
+        # Add new messages
         if self.message_number != 8:
             self.messages.append({"role": role, "content": message})
             self.message_number+=1
-
+        # Remove old message after 8 message
         else:
             self.messages.pop(0)
             self.message_number = 0
             self.messages.append({"role": role, "content": message})
-
+    # Return all stored Messages
     def remind_messages(self):
         return self.messages
